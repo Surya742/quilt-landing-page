a62716a3b677a201f2d2599c35a881a53ff42862
Surya742 <suryakumarsahani.742@gmail.com>
2022-03-22 14:10:32 +0530

    section part in progrerss

--- src/pages/Home/Section.js
+++ src/pages/Home/Section.js
@@ -1,33 +1,67 @@
-    import React, { Component } from 'react'
+import React, { Component } from 'react'
 import { Col, Container, Row } from 'reactstrap';
-import Phone from '../../assets/images/Phone.svg';
+import MsgFrame from '../../assets/images/newUI/Msg-frame.svg';
+import Cube1 from '../../assets/images/newUI/small-1-cube.svg';
+import HollowCube from '../../assets/images/newUI/hollowCube.svg';
+import '../../assets/css/newUI/Section.css';
 
 export default class Section extends Component {
     render() {
         return (
             <React.Fragment>
-                <section className="bg-home d-flex align-items-center">
-            <Container>
-                <Row className="justify-content-center">
-                    <Col lg={6} xs={12}>
-                        <div className="title-heading text-left mt-5 pt-4">
-                            <h4 className="heading fw-bold text-dark title-dark mb-3">Quilt is <span className='hero-span'>secure</span> social trading and <span className='hero-span'>messaging</span> application, built on <span className='hero-span bg-text'>blockchain</span></h4>
-                            <h5 className="para-desc mx-auto text-success title-dark">The Future of Social Media in your hands</h5>
-                            <div className="d-flex justify-content-left">
-                                <a href="https://app.quilt.chat" className="btn btn-success btn-pills">Launch app</a>
-                            </div>
+                <section className="bg-home d-flex align-items-center" style={{backgroundColor: '#212121'}}>
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col lg={5} xs={12}>
+                                <div className="title-heading text-left mt-5 pt-4" >
+                                    <img src={MsgFrame} className="MsgFrameBox" alt="" />
+                                    <h4 className="heading fw-bolder text-light title-light mb-3">Messaging made <span style={{color: '#00E467'}}>Secure</span> on the Blockchain</h4>
+                                </div>
+                            </Col>
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
+                                <img src={HollowCube} className="Hollo1" alt="" />
 
-                        </div>
-                    </Col>
-                    <Col lg={6} xs={12} className="mt-4 pt-2 mt-sm-0 pt-sm-0">
-                <div className="text-md-end text-center ms-lg-4">
-                  <img src={Phone} className="img-fluid" alt="" />
-                </div>
-              </Col>
-                </Row>
-            </Container>
-        </section>
+                                <img src={Cube1} className="Cube1" alt="" />
+                                <img src={Cube1} className="Cube1" alt="" />
+
+                                <img src={Cube1} className="Cube2" alt="" />
+                                <img src={Cube1} className="Cube2" alt="" />
+
+                                <img src={Cube1} className="Cube3" alt="" />
+                                <img src={Cube1} className="Cube3" alt="" />
+
+                                <img src={Cube1} className="Cube4" alt="" />
+                                <img src={Cube1} className="Cube4" alt="" />
+                                
+                                <img src={Cube1} className="Cube5" alt="" />
+                                <img src={Cube1} className="Cube5" alt="" />
+
+                                <img src={Cube1} className="Cube6" alt="" />
+                                <img src={Cube1} className="Cube6" alt="" />
+
+                                <img src={Cube1} className="Cube7" alt="" />
+                                <img src={Cube1} className="Cube7" alt="" />
+                            <Col lg={7} xs={12} className="mt-4 pt-2 mt-sm-0 pt-sm-0">
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
             </React.Fragment>
         )
     }
-}
+}//Messaging made
+// Secure on the Blockchain style={{backgraoundImages:"url( 
